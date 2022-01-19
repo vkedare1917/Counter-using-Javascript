1f9eed55fd158ecd1c8b5bc07d6958d806ab4c6c
Vivek <vpkedare.97@gmail.com>
2022-01-19 20:33:46 +0530

    Project Updated

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let count = 0;
 
 increaseBtn.addEventListener('click', function () {
     count++;
-    value.innerHTML = count;
+    value.textContent = count;
     if (count > 0) {
         value.style.color = "green";
     }
@@ -18,7 +18,7 @@ increaseBtn.addEventListener('click', function () {
 
 decreaseBtn.addEventListener('click', function () {
     count--;
-    value.innerHTML = count;
+    value.textContent = count;
     if (count < 0) {
         value.style.color = "red";
     }
@@ -29,8 +29,57 @@ decreaseBtn.addEventListener('click', function () {
 
 resetBtn.addEventListener('click', function () {
     count = 0;
-    value.innerHTML = count;
+    value.textContent = count;
     if (count === 0) {
         value.style.color = "#000";
     }
+})
+
+const btns = document.querySelectorAll(".btns");
+
+btns.forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+        const btnValue = e.currentTarget.classList;
+        if(btnValue.contains("minus-two")) {
+            count -= 2; 
+            value.textContent = count;
+        }
+        else if(btnValue.contains("minus-three")) {
+            count -= 3; 
+            value.textContent = count;
+        }
+        else if(btnValue.contains("minus-five")) {
+            count -= 5; 
+            value.textContent = count;
+        }
+        else if(btnValue.contains("minus-ten")) {
+            count -= 10; 
+            value.textContent = count;
+        }
+        else if(btnValue.contains("add-two")) {
+            count += 2; 
+            value.textContent = count;
+        }
+        else if(btnValue.contains("add-three")) {
+            count += 3; 
+            value.textContent = count;
+        }
+        else if(btnValue.contains("add-five")) {
+            count += 5; 
+            value.textContent = count;
+        }
+        else if(btnValue.contains("add-ten")) {
+            count += 10; 
+            value.textContent = count;
+        }
+        if (count < 0) {
+            value.style.color = "red";
+        }
+        else if(count > 0) {
+            value.style.color = "green";
+        }
+        else if(count===0) {
+            value.style.color = "black";
+        }
+    })
 })
